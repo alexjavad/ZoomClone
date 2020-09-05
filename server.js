caa47aaab87d030f9f4186e07c2444132df3ba31
alexandertechsolutions <alexandertechsolutions@gmail.com>
2020-09-05 10:45:07 -0400

    updated server.js & room.ejs

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+// const cors = require('cors')
+// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
 });
-const {v4: uuidV4 } = require('uuid')
+const { v4: uuidV4 } = require('uuid');
 
 app.use('/peerjs', peerServer);
 
@@ -14,29 +16,27 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
+  res.redirect(`/${uuidV4()}`)
 })
 
 app.get('/:room', (req, res) => {
-    res.render('room', {roomId: req.params.room})
+  res.render('room', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        console.log("Client Connected: ", roomId, userId)
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId)
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
 
-        // messages
-        socket.on('message', (message) => {
-            //send message to the same room
-            io.to(roomId).emit('createMessage', message)
-        }); 
-        
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
 
 server.listen(process.env.PORT||3030)
