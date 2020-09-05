fd9895088529c79095ca236580b307ace626b4c9
alexandertechsolutions <alexandertechsolutions@gmail.com>
2020-09-05 10:11:52 -0400

    Updated Zoom Client for Controls & PeerJS Server

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 const {v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -20,6 +26,12 @@ io.on('connection', socket => {
         console.log("Client Connected: ", roomId, userId)
         socket.join(roomId)
         socket.to(roomId).broadcast.emit('user-connected', userId)
+
+        // messages
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', message)
+        }); 
         
         socket.on('disconnect', () => {
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
